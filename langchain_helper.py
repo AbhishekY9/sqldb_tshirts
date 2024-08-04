e0428becd4bde527e7f7ad8682f6a0ea3112ea9c
Abhishek Yadav <148570859+AbhishekY9@users.noreply.github.com>
2024-08-05 02:17:54 +0530

    Update langchain_helper.py

--- langchain_helper.py
+++ langchain_helper.py
@@ -1,13 +1,15 @@
-from langchain_community.llms import GooglePalm
-from langchain_community.utilities import SQLDatabase
+from langchain.llms import GooglePalm
+from langchain.utilities import SQLDatabase
 from langchain_experimental.sql import SQLDatabaseChain
 from langchain.prompts import SemanticSimilarityExampleSelector
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import Chroma
 from langchain.prompts import FewShotPromptTemplate
-from langchain.chains.sql_database.prompt import PROMPT_SUFFIX
+from langchain.chains.sql_database.prompt import PROMPT_SUFFIX, _mysql_prompt
 from langchain.prompts.prompt import PromptTemplate
+
 from few_shots import few_shots
+
 import os
 from dotenv import load_dotenv
 
