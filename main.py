d10f4711586c6290e7960512cf27f691586576d8
Abhishek Yadav <148570859+AbhishekY9@users.noreply.github.com>
2024-08-05 02:13:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-from langchain.llms import GooglePalm
-from langchain.utilities import SQLDatabase
-from langchain_experimental.sql import SQLDatabaseChain
-from langchain.prompts import SemanticSimilarityExampleSelector
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.prompts import FewShotPromptTemplate
-from langchain.chains.sql_database.prompt import PROMPT_SUFFIX, _mysql_prompt
-from langchain.prompts.prompt import PromptTemplate
 import streamlit as st
 from langchain_helper import get_few_shot_db_chain
 
