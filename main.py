04d22bf78f59063a659aa55a200b83da2a5a8859
Abhishek Yadav <148570859+AbhishekY9@users.noreply.github.com>
2024-08-05 02:12:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ question = st.text_input("Question: ")
 
 if question:
     chain = get_few_shot_db_chain()
-    response = chain(question)
+    response = chain.run(question)
 
     st.header("Answer")
     st.write(response)
